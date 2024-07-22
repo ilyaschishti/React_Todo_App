f3ed215c15cacfc1da6fd0d39a258165f74b28b1
Muhammad Ilyas Chishti <ilyaschishti143@gmail.com>
2024-07-22 22:36:39 +0500

    Titile changed

--- src/App.jsx
+++ src/App.jsx
@@ -70,16 +70,15 @@ function App() {
     <>
       <Navbar />
       <div className="mx-3 md:container md:mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh] md:w-[35%]">
-        <h1 className="font-bold text-center text-3xl">iTask - Manage your todos at one place</h1>
+        <h1 className="font-bold text-center text-3xl">Manage Your  Todos at one Place</h1>
         <div className="addTodo my-5 flex flex-col gap-4">
           <h2 className="text-2xl font-bold">Add a Todo</h2>
           <div className="flex">
             <input
-            c
               onChange={handleChange}
               value={todo}
               type="text"
-              className="w-full rounded-full px-5 py-1 "
+              className="w-full rounded-full px-5 py-1 " placeholder='Enter Your Todo'
             />
             <button
               onClick={handleAdd}
